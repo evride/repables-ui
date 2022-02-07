8e90e1ebfcb33b7abb61b8e11c59c0ecbaf542c8
Evan Ridenour <eridenour@gmail.com>
2022-02-07 23:46:53 +0000

    Initialize Project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { useState, useEffect } from 'react';
+import Registration from './user-compents/Registration';
+import Login from './user-compents/Login';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
