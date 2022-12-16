e532caaadf9f120933c0dc82066c80800fe8417f
Hashem Sowande Gray <hgray1109@gmail.com>
2022-12-16 23:52:36 +0000

    EncodedURI Component changed fetch uri

--- src/page-components/Search.js
+++ src/page-components/Search.js
@@ -7,11 +7,15 @@ export default function Search() {
 
     const [items, setItems] = useState([])
     const token = useSelector(selectToken)
+
+
+
     const [searchParams] = useSearchParams();
     console.log(searchParams.get("q"))
+    const q = searchParams.get("q")
 
     useEffect(() => {
-        fetch(`https://api.repables.com/items`, {
+        fetch(`https://api.repables.com/search?q=${q}`, {
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -19,8 +23,10 @@ export default function Search() {
           })
           .then((resp) => resp.json())
           .then((data) => {
-              setItems(data);
-          });
+                if(Array.isArray(data.results)){
+                    setItems(data.results);
+                }
+          })
       }, []);
 
 const mappedItems = items.map(item => { 
@@ -28,7 +34,6 @@ const mappedItems = items.map(item => {
     <div key={item.id}> 
         <Link to ={"/r/" + item.id}> {item.id}
             <div>
-                <h1>Search Page!!!!!</h1>
                 <h4>{item.user.username}</h4> 
                 <h1>{item.name}</h1>
             </div>
@@ -39,6 +44,7 @@ const mappedItems = items.map(item => {
 
   return (
     <div>
+        <h1>Search Page!!!!!</h1>
         {mappedItems}
     </div>
   );
