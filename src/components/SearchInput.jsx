238f8ebb4b273619688095efa3cc5a2ce44122b5
Hashem Sowande Gray <hgray1109@gmail.com>
2022-12-21 23:54:33 +0000

    Conditionally render Search Bar, Params loaded into input

---
+++ src/components/SearchInput.jsx
@@ -0,0 +1,39 @@
+import React, {useEffect, useRef, useState} from "react"
+import {useNavigate, useSearchParams} from 'react-router-dom'
+
+export default function SearchInput() {
+    const navigate = useNavigate();
+    const inputRef = useRef()
+    const [searchParams] = useSearchParams();
+    const q = searchParams.get("q")
+    const [searchState, setSearchState] = useState("")
+    
+    
+    function handleSubmit(event) {
+          event.preventDefault();
+          const searchVariable = event.target.search.value
+          navigate("/search?q=" + encodeURIComponent(searchVariable).replace( '%20', '+'))  
+       }
+
+    function handleChange(event) {
+        setSearchState(event.target.value)
+    }
+      
+       useEffect(() => {
+          inputRef.current.value = q;
+          inputRef.current.focus();
+          setSearchState(q)
+       },[])
+      
+    
+    return (
+        <form onSubmit={handleSubmit}>
+                <input ref={inputRef} type="text" placeholder="search" name="search" onChange={handleChange}/> 
+                { searchState.length >= 1 ? 
+                (<button>Search!</button>) :
+                null}
+        </form>
+
+
+    )
+}
