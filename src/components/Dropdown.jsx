3b674a33b219345a8051176668daaa88231592aa
Hashem Sowande Gray <hgray1109@gmail.com>
2022-12-30 00:12:13 +0000

    cleaned up errors, modified linter rules

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useRef } from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 import "./Dropdown.scoped.scss" 
 
 
 
+
 export default function Dropdown({ options, children}){
+    
     
     const [open, setOpen] = useState(false);
     const ref = useRef(null)
@@ -31,7 +31,7 @@ export default function Dropdown({ options, children}){
     }, [open]);
 
     return(
-        <div className="dropdown" onClick={clickedDropdown} ref={ref}>
+        <div role="menu" tabIndex={0} className="dropdown" onClick={clickedDropdown} ref={ref}>
             {children}
            { open && Array.isArray(options) && (
                     <ul>
@@ -39,4 +39,12 @@ export default function Dropdown({ options, children}){
                     </ul>)}
         </div>
     )
+}
+
+Dropdown.propTypes = {
+options: PropTypes.arrayOf(PropTypes.exact({
+    label: PropTypes.string,
+    value: PropTypes.string
+})).isRequired,
+children: PropTypes.arrayOf(PropTypes.element).isRequired
 }
