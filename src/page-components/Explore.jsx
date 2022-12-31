79d795be2dfe775861ff2bdfa6a258b360c305ba
Hashem Sowande Gray <hgray1109@gmail.com>
2022-12-31 00:02:31 +0000

    paginated explore page

--- src/page-components/Explore.jsx
+++ src/page-components/Explore.jsx
@@ -8,9 +8,20 @@ export default function Explore() {
 
     const [items, setItems] = useState([])
     const token = useSelector(selectToken)
+    const [page, setPage] = useState(0)
+    const [limit, setLimit] = useState(20)
+
+    function handleChange () {
+      setPage(page + 1)
+      console.log("pressed")
+    }
+
+    function limitChange(event) {
+      setLimit(Number(event.target.value))
+    }
 
     useEffect(() => {
-        fetch(`https://api.repables.com/items`, {
+        fetch(`https://api.repables.com/items?offset=${page * limit}&limit=${limit}`, {
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -18,31 +29,26 @@ export default function Explore() {
           })
           .then((resp) => resp.json())
           .then((data) => {
-              setItems(data);
+              setItems(data.results);
           });
-      }, []);
+      }, [page, limit]);
 
-const mappedItems = items.map(item => { 
-    return ( 
-        <ItemCard item={item} key={item.id}/>
-
-
-    // <div key={item.id}> 
-    //     <Link to ={"/r/" + item.id} className="item-card">
-    //         <div>
-    //             <img src={`https://dev.repables.com/${item.previewImage.large.url}`}
-    //              alt={item.name}/>
-    //             <h1>{item.name}</h1>
-    //             <h4>{item.user.username}</h4> 
-    //         </div>
-    //     </Link> 
-    // </div>
+    const mappedItems = items.map(item => { 
+        return ( 
+         <ItemCard item={item} key={item.id}/>
     );
 })
 
   return (
     <div className="item-container">
+     <select onChange={limitChange}>
+        <option value="10" >10</option>
+        <option value="20" selected>20</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+     </select>
      {mappedItems}
+     <button type="button" onClick={handleChange}>Next page</button>
     </div>
   );
 }
