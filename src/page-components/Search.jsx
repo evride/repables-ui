968f053b3d0902142146f2c40ee68216956d766d
Hashem Sowande Gray <hgray1109@gmail.com>
2023-01-07 19:34:01 +0000

    add paginated functionality from explore to search page

--- src/page-components/Search.jsx
+++ src/page-components/Search.jsx
@@ -1,21 +1,51 @@
 import React, {useEffect, useState} from 'react';
-import {Link, useSearchParams} from 'react-router-dom';
+import { useSearchParams, useNavigate,} from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectToken } from '../store/auth/selectors';
+import ItemCard from '../item-components/ItemCard';
 
 export default function Search() {
 
     const [items, setItems] = useState([])
     const token = useSelector(selectToken)
-
-
-
     const [searchParams] = useSearchParams();
-    console.log(searchParams.get("q"))
     const q = searchParams.get("q")
+    const searchPage = searchParams.get("page")
+    const [limit, setLimit] = useState(20)
+    const [count, setCount] = useState()
+   
+    const page = Number(searchPage ?? 1)  
+    const navigate = useNavigate()
+
+    function setPage(num){
+        navigate(`/search?q=${q}&page=${num}`)
+      }
+      
+      function nextPage () {
+        setPage(page + 1)
+        console.log("pressed")
+      }
+      
+      function previousPage() {
+        if(page !== 1){
+          setPage(page -1)
+        }
+      }
+      
+      
+      function limitChange(event) {
+        const currentPage = page;
+        const currentLimit = limit;
+        const newLimit = event.target.value;
+        const newPage = Math.floor((currentPage - 1) * currentLimit / newLimit ) + 1
+  
+        setLimit(Number(event.target.value))
+        setPage(newPage)
+  
+      }
 
     useEffect(() => {
-        fetch(`https://api.repables.com/search?q=${q}`, {
+        fetch(`https://api.repables.com/search?q=${q}&offset=${(page -1) * limit}&limit=${limit}`, {
             method: 'GET',
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -23,29 +53,52 @@ export default function Search() {
           })
           .then((resp) => resp.json())
           .then((data) => {
-                if(Array.isArray(data.results)){
-                    setItems(data.results);
-                }
+              setItems(data.results);
+              setCount(data.count) 
           })
-      }, []);
+      }, [q, page, limit]);
 
 const mappedItems = items.map(item => { 
     return ( 
-    <div key={item.id}> 
-        <Link to ={"/r/" + item.id}> {item.id}
-            <div>
-                <h4>{item.user.username}</h4> 
-                <h1>{item.name}</h1>
-            </div>
-        </Link> 
-    </div>
+        <ItemCard item={item} key={item.id}/>
     );
 })
 
+const array = [
+    page - 4, page - 3, page - 2, page - 1, page, page + 1, page + 2, page + 3, page + 4
+  ]
+
+  const filteredArray = array.filter(num => {
+   return num >= 1 && num <= count / limit + 1 
+  })
+
+
+  const mappedArray = filteredArray.map(num => {
+  return <button onClick={() => setPage(num)} className={`paginated-numbers ${num === page ? "current-page" : ""}`} type="button" key={num}>{num}</button>
+    } 
+  )
+
   return (
     <div>
-        <h1>Search Page!!!!!</h1>
-        {mappedItems}
+        <div className="results-per-page-container">
+            <label htmlFor="ResultsPerPage">Results per page</label>
+                <select id="ResultsPerPage" className="select-limit" onChange={limitChange} defaultValue="20">
+                <option value="10" >10</option>
+                <option value="20">20</option>
+                <option value="50">50</option>
+                <option value="100">100</option>
+            </select>
+        </div>
+        <div className="item-container">
+            {mappedItems}
+        </div>
+        <div className="results-btn-container">
+          { page !== 1 && <button className="results-page-btn" type="button" onClick={previousPage}>Previous page</button>}
+          <div>
+            {mappedArray}
+            </div>
+         { page < Math.floor(count / limit + 1) && <button className="results-page-btn" type="button" onClick={nextPage}>Next page</button>}
+        </div>
     </div>
   );
 }
